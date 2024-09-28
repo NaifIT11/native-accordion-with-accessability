81145bb0b6993b645f9ed6464e70b4a4ab62888a
Naif Otibi <nn.n@outlook.cl>
2024-09-28 10:23:01 +0300

    adding animation for height with min-height and state for arrow icon

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 const accordionItems = document.querySelectorAll(".accordions__item");
 const accordionTriggers = document.querySelectorAll(".accordion-trigger");
 const accordionContents = document.querySelectorAll(".accordion-content");
-
+const arrowIcons = document.querySelectorAll(".arrow-icon");
 
 accordionTriggers.forEach((at , index) => {
     at.addEventListener("click" , () => {
@@ -13,12 +13,14 @@ accordionTriggers.forEach((at , index) => {
                 at.setAttribute("aria-expanded" , 'false');
                 at.setAttribute("data-state" , 'closed')
                 accordionItems[index].classList.remove("open");
-                accordionContents[index].setAttribute("data-state" , "closed")
+                accordionContents[index].setAttribute("data-state" , "closed");
+                arrowIcons[index].classList.remove("open");
             }
         })
         at.setAttribute("aria-expanded" , 'true');
         at.setAttribute("data-state" , 'opened')
         accordionItems[index].classList.add("open");
-        accordionContents[index].setAttribute("data-state" , "opened")
+        accordionContents[index].setAttribute("data-state" , "opened");
+        arrowIcons[index].classList.add('open');
     });
 })
